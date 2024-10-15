e1b7824efde431b3aec937ace97ca96a2709e74d
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-10-15 21:51:11 +0200

    push

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,7 +9,11 @@ const db_1 = __importDefault(require("./db/db"));
 const openaiRoutes_1 = __importDefault(require("./routes/openaiRoutes"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "https://parent-buddy-three.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.use(express_1.default.json());
 const PORT = process.env.PORT || 6006;
 (0, db_1.default)();
